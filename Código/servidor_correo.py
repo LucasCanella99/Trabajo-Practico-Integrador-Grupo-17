2399048aabf012ab1c88cb3955f37e8375c6834c
LucasCanella99 <lucascanella19@gmail.com>
2025-09-16 12:04:02 -0300

    Modifique el servidor

--- Código/servidor_correo.py
+++ Código/servidor_correo.py
@@ -1,40 +1,36 @@
 from mensaje import Mensaje
 from usuario import Usuario
-# from usuario import Usuario
-# from carpeta import Carpeta
+
 
 class ServidorCorreo:
     def __init__(self):
-        self.__usuarios = {} #Acá vamos a guardar los usuarios, con su nombre y mail.
-        self.__mensajes = [] #Acá vamos a guardar la lista de mensajes.
+        self.__usuarios = {} #Acá vamos a guardar los usuarios, con su correo
 
-    def registrar_usuario(self,nombre,correo):
+    def registrar_usuario(self,nombre,apellido, contraseña,correo):
         if correo in self.__usuarios:
             raise ValueError('La dirección de correo ya existe')
         
-        usuario_nuevo = Usuario(nombre,correo)
+        usuario_nuevo = Usuario(nombre,apellido, contraseña,correo)
         self.__usuarios[correo] = usuario_nuevo# elegimos un diccionario porque podemos buscar por la clave directamente y es mas rapido que recorrer una lista entera
-        return 'El usuario: ' + str(nombre) + 'correo: ' + str(correo) +' Se ha registrado exitosamente.'
+        return 'El usuario: ' + str(nombre) + str(apellido) + 'correo: ' + str(correo) +' Se ha registrado exitosamente.'
     
+    def get_usuarios(self):
+        return self.__usuarios.copy() #Por seguridad para que no haya modificaciones accidentales en el diccionario original
 
-    def enviar_mensaje(self,mensaje):
-        pass
+    def enviar_mensaje(self,mensaje,destinatario,remitente,asunto):
+        if destinatario not in self.__usuarios:
+            raise ValueError('El destinatario no esta registrado en el sistema')
+        if remitente not in self.__usuarios:
+            raise ValueError('El remitente no esta registrado en el sistema')
+        destinatario = self.__usuarios[destinatario]#Obtenemos los objeto Usuario de destinatario y abajo de remitente
+        remitente = self.__usuarios[remitente]
+        mensaje_a_enviar = Mensaje(mensaje,destinatario.correo,remitente.correo,asunto)#Se escribe el mensaje con todo lo que requiere
+        remitente.guardar_mensaje_enviado(mensaje_a_enviar)#Guardamos en la bandeja de salida
+        destinatario.guardar_mensaje_recibido(mensaje_a_enviar)#Lo guardamos en la bandeja de entrada del destinatario
 
-    def recibir_mensaje(self):
-        pass
 
-    def listar_mensajes(self,correo):
-        if correo not in self.__usuarios:#verificamos que el correo para listar los mensajes exista
-            raise ValueError('El usuario no existe.')
-        usuario = self.__usuarios[correo] #accedemos al usuario en la lista(Ya registrado)
-        bandeja_de_entrada = usuario.get_bandeja_entrada()#Accedemos a la bandeja de entrada de Usuario con su getter, que es un objeto de Carpeta (bandeja de entrada)
-        if bandeja_de_entrada is None:
-            raise ValueError('El usuario no tiene una bandeja de entrada')
-        mensajes = bandeja_de_entrada.get_mensajes()#Usamos el getter de Usuario para poder tener los mensajes
-        if not mensajes:
-            print('El usuario no tiene mensajes')
-        else:
-            print(f'Mensajes del usuario: {correo} \n')
-            for mensaje in mensajes:
-                print(mensaje)# usamos el str de la clase Mensaje
+        
+        
+
+
 
